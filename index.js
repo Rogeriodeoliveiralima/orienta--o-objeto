911940de6bc54fee7f0edeb29bba485c4fc3305d
Rogerio Lima <rlimapg@gmail.com>
2024-04-29 19:24:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const cliente1Nome = "Rogerio";
+const cliente1CPF = 11122222190;
+const cliente1Agencia = 2001;
+const cliente1Saldo = 0;
+
+
+const cliente2Nome = "Cristiane";
+const cliente2CPF = 22222190111;
+const cliente2Agencia = 2001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
