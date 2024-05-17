fe2fb66a15031bc2629c5a87571a23bf751e4cc0
Rogerio Lima <rlimapg@gmail.com>
2024-05-16 22:57:08 -0300

    class Cliente

--- Cliente.js
+++ Cliente.js
@@ -12,3 +12,19 @@
     }
 
 }
+// remove
+export class Cliente{
+    nome;
+    _cpf;
+   
+    get cpf(){
+        return this.cpf;
+    }
+
+    constructor(nome,cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+}
+
